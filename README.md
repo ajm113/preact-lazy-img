e001d37c94e1754c447d190572e7f3eb898ea7f3
Andrew M <andrewmcrobb@gmail.com>
2018-05-04 18:35:49 -0700

    Init commit.

---
+++ README.md
@@ -0,0 +1,15 @@
+# preact-lazy-img (WIP)
+
+Lazy load images when the user scrolls to them. Even plays GIFs when in view!
+
+**NOTE: DO NOT USE IN PRODUCTION! STILL WORK IN PROGRESS!!!!**
+
+Foundation based on [react-lazy](https://github.com/Merri/react-lazy)
+
+## Install
+
+```shell
+    $ npm install preact-lazy-img
+```
+
+More comming soon...
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const webpack = require('webpack')
+
+module.exports = {
+    devtool: 'source-map',
+    entry: './dist/module/index.js',
+    output: {
+        path: __dirname + '/dist/umd/',
+        filename: 'react-lazy.js',
+        publicPath: '/build/',
+        library: 'ReactLazy',
+        libraryTarget: 'umd'
+    },
+    module: {
+        loaders: [
+            { test: /\.(js|jsx)?$/,
+                loader: 'babel-loader',
+                exclude: [
+                    '/dist/',
+                    '/node_modules/',
+                    '/test/',
+                ],
+                query: {
+                    presets: [['es2015', { loose: true, modules: false }], 'stage-0']
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            beautify: false,
+            comments: /^$/,
+            compress: {
+                warnings: false
+            },
+            mangle: true,
+            output: {
+                comments: false,
+                semicolons: false
+            },
+            sourceMap: true
+        }),
+        new webpack.optimize.ModuleConcatenationPlugin(),
+    ],
+    externals: [
+        {
+            'prop-types': {
+                root: 'PropTypes',
+                commonjs2: 'prop-types',
+                commonjs: 'prop-types',
+                amd: 'prop-types'
+            }
+        }
+    ],
+}
