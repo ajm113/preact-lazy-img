13290a472a6fbcca67468eddf38963dc3e1fed21
Andrew M <andrewmcrobb@gmail.com>
2018-05-08 04:48:54 -0700

    Updated dist reflecting recent changes.

--- dist/module/index.js
+++ dist/module/index.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.LazyImgGroup = exports.LazyImg = exports.LazyImgWrapper = undefined;
+exports.LazyImg = exports.LazyImgWrapper = undefined;
 
 var _LazyWrapper = require('./components/LazyWrapper');
 
@@ -13,12 +13,7 @@ var _LazyImg2 = require('./components/LazyImg');
 
 var _LazyImg3 = _interopRequireDefault(_LazyImg2);
 
-var _LazyImgGroup2 = require('./components/LazyImgGroup');
-
-var _LazyImgGroup3 = _interopRequireDefault(_LazyImgGroup2);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.LazyImgWrapper = _LazyWrapper2.default;
 exports.LazyImg = _LazyImg3.default;
-exports.LazyImgGroup = _LazyImgGroup3.default;
